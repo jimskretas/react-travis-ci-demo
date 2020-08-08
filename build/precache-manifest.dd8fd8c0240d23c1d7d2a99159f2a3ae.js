3611eacf227008b2b759887800bc5787000d6a3f
Travis <travis@travis-ci.org>
2020-08-08 17:09:04 +0000

    Travis build 18

---
+++ build/precache-manifest.dd8fd8c0240d23c1d7d2a99159f2a3ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ee12b3ea1764200acb8aef48df196b",
+    "url": "/react-travis-ci-demo/index.html"
+  },
+  {
+    "revision": "c0c9c14a264040e0a8a1",
+    "url": "/react-travis-ci-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5d91f2ce5abf7eb3d7b",
+    "url": "/react-travis-ci-demo/static/js/2.5e810c6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-travis-ci-demo/static/js/2.5e810c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c9c14a264040e0a8a1",
+    "url": "/react-travis-ci-demo/static/js/main.043b6037.chunk.js"
+  },
+  {
+    "revision": "9ff56b17b4f4224cf9c6",
+    "url": "/react-travis-ci-demo/static/js/runtime-main.c046d4c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-travis-ci-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
